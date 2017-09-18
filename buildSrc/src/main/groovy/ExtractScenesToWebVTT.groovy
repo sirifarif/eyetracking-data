aa5a4f31da536d291bfd3686b544a0c74f433986
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-09-18 11:34:45 +0200

    format dates as timestamps

--- buildSrc/src/main/groovy/ExtractScenesToWebVTT.groovy
+++ buildSrc/src/main/groovy/ExtractScenesToWebVTT.groovy
@@ -2,6 +2,8 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.*
 import org.yaml.snakeyaml.Yaml
 
+import java.time.*
+
 class ExtractScenesToWebVTT extends DefaultTask {
 
     @InputFile
@@ -17,16 +19,30 @@ class ExtractScenesToWebVTT extends DefaultTask {
             def frameIter = new Yaml().load(srcFile.newReader()).iterator()
             def prevFrame = frameIter.next()
             def frame
+            Instant offset = prevFrame.date.toInstant()
             while (frameIter.hasNext()) {
                 frame = frameIter.next()
                 if (prevFrame.window != frame.window) {
-                    vtt.println "\n$prevFrame.date --> $frame.date"
+                    def start = formatInstantToTimestamp(prevFrame.date.toInstant(), offset)
+                    def end = formatInstantToTimestamp(frame.date.toInstant(), offset)
+                    vtt.println "\n$start --> $end"
                     vtt.println "{ start: $prevFrame.window.start, end: $prevFrame.window.end }"
                     prevFrame = frame
                 }
             }
-            vtt.println "\n$prevFrame.date --> $frame.date"
+            def start = formatInstantToTimestamp(prevFrame.date.toInstant(), offset)
+            def end = formatInstantToTimestamp(frame.date.toInstant(), offset)
+            vtt.println "\n$start --> $end"
             vtt.println "{ start: $frame.window.start, end: $frame.window.end }"
         }
+    }
+
+    String formatInstantToTimestamp(Instant instant, Instant offset) {
+        def duration = Duration.between(offset, instant)
+        [
+                duration.toHours(),
+                duration.toMinutes() - duration.toHours() * 60,
+                duration.seconds - duration.toMinutes() * 60
+        ].collect { "$it".padLeft(2, '0') }.join(':')
     }
 }
