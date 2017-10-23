8b69cbb9082d1fb29c0c2db8b4575978a13cc184
Arif Khan <arif.khan@dkfi.de>
2017-10-23 11:59:00 +0200

    plotting fixations on scene video

--- buildSrc/src/main/groovy/MergeLogs.groovy
+++ buildSrc/src/main/groovy/MergeLogs.groovy
@@ -40,8 +40,8 @@ class MergeLogs extends DefaultTask {
                         timeStamp: Date.parse(dateFormat, it.date),
                         gazeType : it.value.gaze_type,
                         gazeDur  : it.value.gaze_duration as double,
-                        xPos     : it.value.xPos as int,
-                        yPos     : it.value.yPos as int
+                        position : [xPos: it.value.xPos as int,
+                                    yPos: it.value.yPos as int]
                 ]
             }
             sceneData << sceneMap
